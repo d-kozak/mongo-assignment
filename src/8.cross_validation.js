8cd8687e8ee29ae0ff268c31d54975a19a42216f
dkozak <dkozak94@gmail.com>
2019-05-01 13:24:35 +0100

    finish task 8

--- src/8.cross_validation.js
+++ src/8.cross_validation.js
@@ -1,14 +1,24 @@
+db.cross_validation.deleteMany({});
+db.cross_validation_0.deleteMany({});
+db.cross_validation_1.deleteMany({});
+db.cross_validation_2.deleteMany({});
+db.cross_validation_3.deleteMany({});
+db.cross_validation_4.deleteMany({});
+
+// number of reviews
 const N = db.reviews.count();
+// number of categories
 const K = 5;
 
 const map = function () {
-    const k = 5;
-    const category = Math.floor(Math.random() * k);
+    const k = 5; // has to be local, cannot access the "outside"
+    const category = Math.floor(Math.random() * k); // split choose random number  0 - 4
     emit(category, {
         reviews: [this]
     })
 };
 
+// group reviews with same key them together
 const reduce = function (key, values) {
     const reviews = [];
     for (let item of values) {
@@ -25,7 +35,7 @@ db.reviews.mapReduce(
     "cross_validation"
 )
 
-
+// split the "big" cross validation collection into individual collection
 for (let i = 0; i < K; i++) {
     db.cross_validation.aggregate([
         {
